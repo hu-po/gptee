5e4ccb6ae82d4947a206b30ed5c0b3f985eaf129
hu.po <hugo.p.cmu@gmail.com>
2023-11-09 11:13:43 -0600

    wtf is ros

---
+++ gptee/gptee.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from gptee.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+package_name = 'gptee'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=find_packages(),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    entry_points={
+        'console_scripts': [
+            'gptee = gptee.gptee:main'
+        ],
+    },
+)
